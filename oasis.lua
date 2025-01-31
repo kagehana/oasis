9ccc6b0663bece457ddd1a6f8cb92023cf8b4a79
하늘의 한계 <qeoseol@gmail.com>
2025-01-31 00:03:36 -0600

    Update oasis.lua

--- oasis.lua
+++ oasis.lua
@@ -1,1 +1,25 @@
 
+-- bootleg require
+local rawgit = 'https://raw.githubusercontent.com'
+local repo   = 'kagehana/oasis'
+local branch = 'refs/heads/main'
+
+local function get(fname)
+    return loadstring(game:HttpGet('%s/%s/%s/%s.lua'):format(rawgit, repo, branch, fname))
+end
+
+-- tools
+local class = get('class')
+
+-- classes
+local window  = class('window')
+local tab     = class('tab')
+local group   = class('group')
+local toggle  = class('toggle')
+local slider  = class('slider')
+local query   = class('query')
+local text    = class('text')
+local options = class('options')
+local palette = class('palette')
+
+-- to be continued
